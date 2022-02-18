750dfb452cc2a4f7de3c80578e19f2fef32c0b05
olehh3103 <92575053+olehh3103@users.noreply.github.com>
2022-02-18 19:24:44 +0200

    Add files via upload

---
+++ hidden.py
@@ -0,0 +1,10 @@
+# Keep this file separate
+
+# https://apps.twitter.com/
+# Create new App and get the four strings
+
+def oauth():
+    return {"consumer_key": "0PlKlKMbNYWL9UeVuC6DPxX4B",
+            "consumer_secret": "dpvpCGjA9udpjsPkCVfdxZhmAOLMXFeZcZEVLbH3usRiv8fHfN",
+            "token_key": "963011359737774080-D73fzqJ2jocOftSRejq7tJz2gn4Xa2n",
+            "token_secret": "KJ9zSSyrJsYLidt7PQTyPeAqZvrB92bxWwlzv7Ma8hMcG"}
